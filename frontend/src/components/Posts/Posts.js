3959c7b8f6df49ee8046266facf09ae05532317f
pplociennik23 <pplociennik23@gmail.com>
2023-11-22 14:03:58 +0100

    [011] Posts component implementation

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -2,6 +2,7 @@ import React from "react";
 import Post from "./Post/Post.js";
 import useStyles from './styles.js';
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 
 const Posts = () => {
 
@@ -11,12 +12,17 @@ const Posts = () => {
     console.log(posts);
     
     return(
-        <>
-            <h1>POSTS</h1>
-            <Post/>
-            <Post/>
-            <Post/>
-        </>
+            !posts.length ? <CircularProgress/> : (
+            <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}> 
+                {
+                    posts.map((post) => (
+                        <Grid key={post._id} item xs={12} sm={6}>
+                            <Post post={post}/>
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
     );
 }
 
