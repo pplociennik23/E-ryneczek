c97032dae8f391bc8a99bb26c8faa6a0c07f81aa
pplociennik23 <pplociennik23@gmail.com>
2023-12-12 18:42:22 +0100

    [016]  Delete Post functionality (frontend and backend)

--- backend/controllers/Posts.js
+++ backend/controllers/Posts.js
@@ -33,9 +33,19 @@ export const updatePost = async (req, res) => {
 
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    const updatedPost =  await PostMessage.findByIdAndUpdate(_id, post, { new: true });
-    res.json(updatePost);
+    const updatedPost =  await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+    res.json(updatedPost);
 
+}
+
+export const deletePost = async (req, res) => {
+
+    const { id: _id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    await PostMessage.findOneAndDelete({ _id });
+    res.json({ message: 'Post deleted'});
 }
 
 export default router;
--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -5,10 +5,13 @@ import moment from 'moment';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-
+import { useDispatch } from 'react-redux';
+import { deletePost } from '../../../actions/posts.js';
+  
 const Post = ({post, setCurrentId}) => {
 
     const classes = useStyles();
+    const dispatch = useDispatch();
 
     return(
         <Card className={classes.card}>
@@ -35,7 +38,7 @@ const Post = ({post, setCurrentId}) => {
                     Like
                     {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={() =>{}}>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small"/>
                     Delete
                 </Button>
