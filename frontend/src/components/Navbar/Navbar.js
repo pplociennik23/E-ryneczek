91464aedd8331e215a11e9796c257ea9bb8b0bda
pplociennik23 <pplociennik23@gmail.com>
2025-01-06 12:47:11 +0100

    [033] logo img code unified

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,11 +1,11 @@
 import { AppBar } from '@mui/material';
-import eryneczek from '../../images/eryneczek.png';
+import logo from '../../images/logo.png';
 import styles from './Navbar.module.css'
 
 const Navbar = () => {
     return (
         <AppBar className={styles.appBar} position="static" color="inherit">
-                        <img className={styles.image} src={eryneczek} alt="e-ryneczek"/>
+                        <img className={styles.image} src={logo} alt="logo"/>
         </AppBar>
     )
 }
